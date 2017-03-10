bfeb82f2d56cfd965dc365a29a3ec8f9e828c8a9
tocgic <jeongyon.cho@gmail.com>
2017-03-10 10:38:22 +0900

    Use, "Kotlin Android Extensions"
    2. kt code, layout import
    	import kotlinx.android.synthetic.main.{layout-file-name}.*
    	=> import kotlinx.android.synthetic.main.activity_main.*

--- app/src/main/java/kr/pe/tocgic/examkotlin/MainActivity.kt
+++ app/src/main/java/kr/pe/tocgic/examkotlin/MainActivity.kt
@@ -3,6 +3,7 @@ package kr.pe.tocgic.examkotlin
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
