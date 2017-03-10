9ea012286ca3b65ebdde0ea5796a4f1c7ce41f9f
tocgic <jeongyon.cho@gmail.com>
2017-03-10 10:28:07 +0900

    샘플코드 변경
    "Hi, Kotlin!"

--- app/src/main/java/kr/pe/tocgic/examkotlin/MainActivity.kt
+++ app/src/main/java/kr/pe/tocgic/examkotlin/MainActivity.kt
@@ -11,6 +11,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val textView = findViewById(R.id.tvHello) as TextView
-        textView.text = "Hi, Java!"
+        textView.text = "Hi, Kotlin!"
     }
 }
