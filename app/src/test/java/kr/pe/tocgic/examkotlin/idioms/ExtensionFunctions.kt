7d262e42b287b415dc98e2bf1b2f4f67bb1af726
tocgic <jeongyon.cho@gmail.com>
2017-03-27 01:26:38 +0900

    add String.spaceToCamelCase2()

--- app/src/test/java/kr/pe/tocgic/examkotlin/idioms/ExtensionFunctions.kt
+++ app/src/test/java/kr/pe/tocgic/examkotlin/idioms/ExtensionFunctions.kt
@@ -28,10 +28,37 @@ class ExtensionFunctions {
         return result.toString()
     }
 
+    fun String.spaceToCamelCase2(): String = String(
+        mapIndexed {
+                index, c -> if (index > 0 && this[index - 1] == ' ') c.toUpperCase() else c
+                }.
+                filter { it != ' ' }.toCharArray()
+    )
+
     @Test
     fun main() {
         println("Convertthistocamelcase".spaceToCamelCase())
         println("Convert this to camelcase".spaceToCamelCase())
         println("Convert  this  to  camelcase".spaceToCamelCase())
+        println("Convertthistocamelcase".spaceToCamelCase2())
+        println("Convert this to camelcase".spaceToCamelCase2())
+        println("Convert  this  to  camelcase".spaceToCamelCase2())
+
+        val limit = 10000
+        var count = 0
+        val startTime = System.currentTimeMillis()
+        while (count++ < limit) {
+            "Convert  this  to  camelcase".spaceToCamelCase()
+        }
+        val processedTime = System.currentTimeMillis() - startTime
+        count = 0
+        val startTime2 = System.currentTimeMillis()
+        while (count++ < limit) {
+            "Convert  this  to  camelcase".spaceToCamelCase2()
+        }
+        val processedTime2 = System.currentTimeMillis() - startTime2
+        println("processedTime:$processedTime")
+        println("processedTime2:$processedTime2")
+
     }
 }
